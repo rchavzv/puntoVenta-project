ca675686df72b091fdba48cc7d45f5295ee8291c
rchavzv <rchavzv@gmail.com>
2026-06-04 00:35:22 -0600

    Pantalla login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Login } from './pages/login/login';
+export const routes: Routes = [
+  {
+    path: '',
+    component: Login,
+  },
+];
---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {}
